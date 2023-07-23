99ed587d2de901acd4b20d87309cc866775192c8
Rushi Gawali <rushi_g@tekditechnologies.com>
2023-07-23 12:10:02 +0530

    script changes in bulk issuance

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,16 +6,17 @@ import { HttpService } from '@nestjs/axios';
 //generate fake words
 const { faker } = require('@faker-js/faker');
 
+//file stream
+const fs = require('fs');
+
 @Console()
 export class AppService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(public readonly httpService: HttpService) {}
   @Command({
     command: 'ulp',
   })
   async ulpConsole(): Promise<void> {
     const moment = require('moment');
-    //file stream
-    const fs = require('fs');
     //read and write csv
     const csvParser = require('csv-parser');
     const { stringify } = require('csv-stringify');
@@ -34,8 +35,6 @@ export class AppService {
     let ULPCLI_VERSION = null;
     let ULPCLI_NAME = null;
     let BULK_ISSUANCE_URL = null;
-    let DEFAULT_PASSWORD = null;
-    let CLIENT_USERNAME = null;
     let EWALLET_URL = null;
     let VERIFICATION_URL = null;
     let envs = await new Promise((done) =>
@@ -43,16 +42,12 @@ export class AppService {
         ULPCLI_VERSION = process.env.ULPCLI_VERSION;
         ULPCLI_NAME = process.env.ULPCLI_NAME;
         BULK_ISSUANCE_URL = process.env.BULK_ISSUANCE_URL;
-        DEFAULT_PASSWORD = process.env.DEFAULT_PASSWORD;
-        CLIENT_USERNAME = process.env.CLIENT_USERNAME;
         EWALLET_URL = process.env.EWALLET_URL;
         VERIFICATION_URL = process.env.VERIFICATION_URL;
         done({
           ULPCLI_VERSION: ULPCLI_VERSION,
           ULPCLI_NAME: ULPCLI_NAME,
           BULK_ISSUANCE_URL: BULK_ISSUANCE_URL,
-          DEFAULT_PASSWORD: DEFAULT_PASSWORD,
-          CLIENT_USERNAME: CLIENT_USERNAME,
           EWALLET_URL: EWALLET_URL,
           VERIFICATION_URL: VERIFICATION_URL,
         });
@@ -67,8 +62,7 @@ export class AppService {
     );
     let client_response = await new Promise<any>(async (done) => {
       const data = JSON.stringify({
-        username: CLIENT_USERNAME,
-        password: DEFAULT_PASSWORD,
+        password: 'test@4321',
       });
       const url = BULK_ISSUANCE_URL + '/bulk/v1/clienttoken';
       const config: AxiosRequestConfig = {
@@ -102,7 +96,7 @@ export class AppService {
       );
     } else {
       spin.succeed('Got Client Token.');
-      let client_token = client_response.result;
+      let client_token = client_response.token;
       console.log(
         JSON.stringify(
           {
@@ -168,9 +162,6 @@ export class AppService {
         const issuer_data = {
           name: ISSUER_ID,
           did: ISSUER_DID,
-          email: ISSUER_ID + '@gmail.com',
-          mobile: '9999999999',
-          userid: ISSUER_ID,
         };
         const data = JSON.stringify(issuer_data);
         let issuer_invite_response = await new Promise<any>(async (done) => {
@@ -211,6 +202,247 @@ export class AppService {
         } else {
           spin.succeed('Issuer information stored successfully.');
           console.log(JSON.stringify(issuer_data, null, '\t'));
+
+          //create sample schema
+          spin.start(`Getting Sample Schema...`);
+          let UNIQUE_SCHEMA_TAG =
+            'unique_schema_' + Math.floor(Math.random() * 100000 + 1);
+          let create_schema_list = await new Promise<any>(async (done) => {
+            //get create_schema_list
+            try {
+              let sample_schema_dir = __dirname + '/sample_schemas/';
+              await this.readFiles(
+                this.httpService,
+                sample_schema_dir,
+                async function (httpService: HttpService, filename, content) {
+                  //console.log(filename);
+                  //console.log(content);
+                  let file_json = JSON.parse(content);
+                  //generate did
+                  let UNIQUE_ID =
+                    'unique_' + Math.floor(Math.random() * 1000 + 1);
+                  let did_response = await new Promise<any>(
+                    async (done_did) => {
+                      const data = JSON.stringify({
+                        uniquetext: UNIQUE_ID,
+                      });
+                      const url = BULK_ISSUANCE_URL + '/bulk/v1/getdid';
+                      const config: AxiosRequestConfig = {
+                        headers: {
+                          'Content-Type': 'application/json',
+                        },
+                      };
+                      //console.log(data);
+                      let response_data = null;
+                      try {
+                        //let httpService = HttpService;
+                        const observable = httpService.post(url, data, config);
+                        const promise = observable.toPromise();
+                        const response = await promise;
+                        response_data = response.data;
+                      } catch (e) {
+                        console.log('error' + e);
+                        response_data = { error: e };
+                      }
+                      done_did(response_data);
+                    },
+                  );
+                  //console.log(JSON.stringify(did_response));
+                  if (did_response?.error || did_response?.success === false) {
+                  } else {
+                    let UNIQUE_DID = did_response.result;
+                    //create schema
+                    file_json.schema.id = UNIQUE_DID;
+                    //UNIQUE_SCHEMA_TAG
+                    file_json.tags.push(UNIQUE_SCHEMA_TAG);
+                    //console.log(file_json);
+                    //create schema
+                    let schema_response = await new Promise<any>(
+                      async (done_schema) => {
+                        const data = JSON.stringify(file_json);
+                        const url =
+                          BULK_ISSUANCE_URL +
+                          '/bulk/v1/credential/schema/create';
+                        const config: AxiosRequestConfig = {
+                          headers: {
+                            'Content-Type': 'application/json',
+                          },
+                        };
+                        //console.log(data);
+                        let response_data = null;
+                        try {
+                          //let httpService = HttpService;
+                          const observable = httpService.post(
+                            url,
+                            data,
+                            config,
+                          );
+                          const promise = observable.toPromise();
+                          const response = await promise;
+                          response_data = response.data;
+                        } catch (e) {
+                          console.log('error' + e);
+                          response_data = { error: e };
+                        }
+                        done_schema(response_data);
+                      },
+                    );
+                    //console.log(schema_response);
+                  }
+                  //sample_schema.push({ filename: filename, content: content });
+                },
+                function (err) {
+                  console.log(err);
+                  throw err;
+                },
+                function (sample_schema) {
+                  //end cerate schema list
+                  //console.log(sample_schema);
+                  done([]);
+                },
+              );
+            } catch (e) {
+              console.log(e);
+            }
+          });
+          //console.log(create_schema_list);
+          if (create_schema_list === null) {
+            spin.fail('Failed to get Sample Schema');
+          } else {
+            spin.succeed('Created Sample Schema');
+
+            //console.log(JSON.stringify(create_schema_list, null, '\t'));
+            spin.start(`Getting Schema List of tag...${UNIQUE_SCHEMA_TAG}`);
+
+            //aading 3 seconds wait for gettign created schema
+            await this.delay(3000);
+
+            //console.log('UNIQUE_SCHEMA_TAG', UNIQUE_SCHEMA_TAG);
+            let scheam_tag_text = UNIQUE_SCHEMA_TAG;
+            let schema_response = await new Promise<any>(async (done) => {
+              var data = JSON.stringify({
+                taglist: scheam_tag_text,
+              });
+              const url = BULK_ISSUANCE_URL + '/bulk/v1/credential/schema/list';
+              const config: AxiosRequestConfig = {
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+              };
+              //console.log(data);
+              let response_data = null;
+              try {
+                const observable = this.httpService.post(url, data, config);
+                const promise = observable.toPromise();
+                const response = await promise;
+                response_data = response.data;
+                //console.log(response_data);
+              } catch (e) {
+                response_data = { error: e };
+              }
+              done(response_data);
+            });
+            if (schema_response?.error || schema_response?.success === false) {
+              spin.fail('Failed to get schema list');
+              console.log(
+                JSON.stringify(
+                  schema_response?.error
+                    ? schema_response.error
+                    : schema_response?.result
+                    ? schema_response.result
+                    : {},
+                  null,
+                  '\t',
+                ),
+              );
+            } else {
+              spin.succeed(`Got Schema List of tag...${UNIQUE_SCHEMA_TAG}`);
+              let schema_list = schema_response.result;
+              console.log(
+                JSON.stringify(
+                  {
+                    'Schema List': schema_list,
+                  },
+                  null,
+                  '\t',
+                ),
+              );
+              //do untill all schemas data created
+              for (
+                let i_schema = 0;
+                i_schema < schema_list.length;
+                i_schema++
+              ) {
+                console.log('#################');
+                console.log(schema_list[i_schema].schema_name);
+                console.log('#################');
+                //get first schema
+                spin.start(
+                  `Getting Required Fileds from ...${schema_list[i_schema].schema_name} schema`,
+                );
+                let schema_filed_response = await new Promise<any>(
+                  async (done) => {
+                    const data = JSON.stringify({
+                      schema_id: schema_list[i_schema].schema_id,
+                    });
+                    const url =
+                      BULK_ISSUANCE_URL + '/bulk/v1/credential/schema/fields';
+                    const config: AxiosRequestConfig = {
+                      headers: {
+                        'Content-Type': 'application/json',
+                      },
+                    };
+                    let response_data = null;
+                    try {
+                      const observable = this.httpService.post(
+                        url,
+                        data,
+                        config,
+                      );
+                      const promise = observable.toPromise();
+                      const response = await promise;
+                      response_data = response.data;
+                    } catch (e) {
+                      response_data = { error: e };
+                    }
+                    done(response_data);
+                  },
+                );
+                if (
+                  schema_filed_response?.error ||
+                  schema_filed_response?.success === false
+                ) {
+                  spin.fail('Failed to get schema fields');
+                  console.log(
+                    JSON.stringify(
+                      schema_filed_response?.error
+                        ? schema_filed_response.error
+                        : schema_filed_response?.result
+                        ? schema_filed_response.result
+                        : {},
+                      null,
+                      '\t',
+                    ),
+                  );
+                } else {
+                  spin.succeed(
+                    `Got Schema Required Fields...${schema_list[i_schema].schema_name}`,
+                  );
+                  console.log(
+                    JSON.stringify(
+                      {
+                        'Schema Field List': schema_filed_response,
+                      },
+                      null,
+                      '\t',
+                    ),
+                  );
+                }
+              }
+            }
+          }
+
+          /*
           //issuer token
           spin.start(`Getting Issuer Token...`);
           let issuer_response = await new Promise<any>(async (done) => {
@@ -557,9 +789,44 @@ export class AppService {
                 }
               }
             }
-          }
+          }*/
         }
       }
     }
   }
+  async readFiles(
+    httpService: HttpService,
+    dirname,
+    onFileContent,
+    onError,
+    onComplete,
+  ) {
+    let count = 0;
+    let sample_schema = [];
+    fs.readdir(dirname, function (err, filenames) {
+      //console.log('dirname', dirname);
+      if (err) {
+        console.log(err);
+        onError(err);
+        return;
+      }
+      filenames.forEach(function (filename) {
+        count++;
+        fs.readFile(dirname + filename, 'utf-8', function (err, content) {
+          //console.log('filename', dirname + filename);
+          if (err) {
+            console.log(err);
+            onError(err);
+            return;
+          }
+          sample_schema.push({ filename: filename, content: content });
+          onFileContent(httpService, filename, content);
+        });
+        if (filenames.length === count) {
+          onComplete(sample_schema);
+        }
+      });
+    });
+  }
+  delay = (ms) => new Promise((res) => setTimeout(res, ms));
 }
